a59bfd85fc56b71e3bc00d0d86d0bccbedc6fe30
Martin Perussini <mperussini@frsf.utn.edu.ar>
2021-05-25 19:38:37 -0300

    Artemis Recepcion de mensajes

--- src/main/java/dan/tp2021/productos/services/MovimientoStockServiceImpl.java
+++ src/main/java/dan/tp2021/productos/services/MovimientoStockServiceImpl.java
@@ -1,20 +1,33 @@
 package dan.tp2021.productos.services;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.logging.Logger;
 
+import dan.tp2021.productos.domain.DetalleProvision;
+import dan.tp2021.productos.domain.Material;
+import dan.tp2021.productos.domain.Provision;
+import io.swagger.models.auth.In;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.JmsException;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
 import dan.tp2021.productos.dao.MovimientosStockInMemoryRepository;
 import dan.tp2021.productos.domain.MovimientosStock;
+
+import javax.jms.Message;
 
 @Service
 public class MovimientoStockServiceImpl implements MovimientosStockService {
 
 	@Autowired
 	MovimientosStockInMemoryRepository inMemoryRepository;
+
+	@Autowired
+	MaterialService materialServiceImpl;
+
+	@Autowired
+	ProvisionService provisionServiceImpl;
 
 	@Override
 	public MovimientosStock getMovimientoStockById(Integer id) throws MovimientosStockException {
@@ -78,6 +91,43 @@ public class MovimientoStockServiceImpl implements MovimientosStockService {
 		inMemoryRepository.deleteById(id);
 
 		return find.get();
+	}
+
+	//Recibo mensajes del microservicio de Pedidos
+	//Supongo que la descripcion en del producto en el microservicio de Pedidos equivale al nombre
+	//en este microservicio
+	@JmsListener(destination = "COLA_PEDIDOS")
+	public void handle(HashMap msg) throws JmsException{
+		msg.forEach((k,v)-> {
+			try {
+				registrarMovStock(k.toString(),(Integer) v);
+			} catch (MaterialService.MaterialException | ProvisionService.ProvisionException | MovimientosStockException e) {
+				e.printStackTrace();
+			}
+		});
+	}
+
+	private void registrarMovStock(String idString, Integer cantidad) throws MaterialService.MaterialException, ProvisionService.ProvisionException, MovimientosStockException {
+		Integer id = Integer.valueOf(idString);
+		MovimientosStock movStockNuevo = new MovimientosStock();
+		Material material = materialServiceImpl.getMaterialById(id);
+		//Actualizo stock actual del material
+		material.setStockActual(material.getStockActual()-cantidad);
+		materialServiceImpl.saveMaterial(material);
+		movStockNuevo.setMaterial(material);
+		movStockNuevo.setCantidadSalida(cantidad);
+		//Si el nuevo stock es menor al minimo creo una provision
+		if(material.getStockActual()<material.getStockMinimo()){
+			Provision provision = new Provision();
+			DetalleProvision detalleProvision = new DetalleProvision();
+			detalleProvision.setMaterial(material);
+			//Compro lo justo para quedar en el minimo, esto se podria variar segun necesidad del negocio
+			detalleProvision.setCantidad(material.getStockMinimo()-material.getStockActual());
+			provision.getDetalle().add(detalleProvision);
+			provisionServiceImpl.saveProvision(provision);
+			movStockNuevo.setDetalleProvision(detalleProvision);
+		}
+		this.saveMovimientoStock(movStockNuevo);
 	}
 
 }
